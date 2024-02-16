8fb24d9065efa7d3d87031959e28c8922521e8f6
Hasan Hasanzade <h.hesen@hotmail.com>
2024-02-16 11:22:11 +0400

    add configuration for all payment methods

--- Util/PaymentMethod.php
+++ Util/PaymentMethod.php
@@ -5,10 +5,35 @@ namespace BetterPayment\Core\Util;
 class PaymentMethod
 {
     public const CREDIT_CARD = 'betterpayment_credit_card';
+    public const GIROPAY = 'betterpayment_giropay';
+    public const PAYDIREKT = 'betterpayment_paydirekt';
+    public const SOFORT = 'betterpayment_sofort';
+    public const REQUEST_TO_PAY = 'betterpayment_request_to_pay';
+    public const PAYPAL = 'betterpayment_paypal';
     public const SEPA_DIRECT_DEBIT = 'betterpayment_sepa_direct_debit';
+    public const SEPA_DIRECT_DEBIT_B2B = 'betterpayment_sepa_direct_debit_b2b';
+    public const INVOICE = 'betterpayment_invoice';
+    public const INVOICE_B2B = 'betterpayment_invoice_b2b';
+
+    public const ASYNC_PAYMENT_METHODS = [
+        self::CREDIT_CARD,
+        self::GIROPAY,
+        self::PAYDIREKT,
+        self::SOFORT,
+        self::REQUEST_TO_PAY,
+        self::PAYPAL
+    ];
 
     public const SHORTCODE = [
         self::CREDIT_CARD => 'cc',
-        self::SEPA_DIRECT_DEBIT => 'dd'
+        self::GIROPAY => 'giro',
+        self::PAYDIREKT => 'paydirekt',
+        self::SOFORT => 'sofort',
+        self::REQUEST_TO_PAY => 'rtp',
+        self::PAYPAL => 'paypal',
+        self::SEPA_DIRECT_DEBIT => 'dd',
+        self::SEPA_DIRECT_DEBIT_B2B => 'dd_b2b',
+        self::INVOICE => 'kar',
+        self::INVOICE_B2B => 'kar_b2b',
     ];
 }
