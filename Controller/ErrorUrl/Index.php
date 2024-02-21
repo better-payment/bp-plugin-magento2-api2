fd838cd84e9af283d7b7393e6fd1fa1a1ba9345b
Hasan Hasanzade <h.hesen@hotmail.com>
2024-02-21 13:54:58 +0400

    set status to canceled when async payment method failed

--- Controller/ErrorUrl/Index.php
+++ Controller/ErrorUrl/Index.php
@@ -2,19 +2,26 @@
 
 namespace BetterPayment\Core\Controller\ErrorUrl;
 
+use Magento\Checkout\Model\Session;
 use Magento\Framework\App\Action\HttpGetActionInterface;
 use Magento\Framework\Controller\Result\RedirectFactory;
 use Magento\Framework\Message\ManagerInterface;
+use Magento\Sales\Model\Order;
 
 class Index implements HttpGetActionInterface
 {
     private RedirectFactory $redirectFactory;
     private ManagerInterface $messageManager;
+    private Session $session;
 
-    public function __construct(RedirectFactory $redirectFactory, ManagerInterface $messageManager)
-    {
+    public function __construct(
+        RedirectFactory $redirectFactory,
+        ManagerInterface $messageManager,
+        Session $session
+    ) {
         $this->redirectFactory = $redirectFactory;
         $this->messageManager = $messageManager;
+        $this->session = $session;
     }
 
     /**
@@ -22,7 +29,12 @@ class Index implements HttpGetActionInterface
      */
     public function execute()
     {
-        $this->messageManager->addErrorMessage(__('Payment failed in Payment Gateway page'));
-        return $this->redirectFactory->create()->setPath('checkout/cart');
+        $order = $this->session->getLastRealOrder();
+        $order->setStatus(Order::STATE_CANCELED)->setState(Order::STATE_CANCELED);
+        // TODO: add something to history as well
+        $order->save(); // TODO: change this to Save via Repository code
+
+        $this->messageManager->addErrorMessage(__('Payment failed in Payment Gateway page. Try to reorder again.'));
+        return $this->redirectFactory->create()->setPath('sales/order/history');
     }
 }
