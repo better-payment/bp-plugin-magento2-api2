66f251cbb7142c5b7ca8ce388daf533eddc42172
Hasan Hasanzade <h.hesen@hotmail.com>
2024-08-16 17:45:11 +0400

    add partial and full capture feature

---
+++ Gateway/Http/Client/Capture.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace BetterPayment\Core\Gateway\Http\Client;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Psr7\Request;
+use Magento\Payment\Gateway\Http\ClientException;
+use Magento\Payment\Gateway\Http\ClientInterface;
+use Magento\Payment\Gateway\Http\TransferInterface;
+
+class Capture implements ClientInterface
+{
+    private Client $client;
+
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function placeRequest(TransferInterface $transferObject)
+    {
+        $request = new Request(
+            'POST',
+            $transferObject->getUri() . '/rest/capture',
+            $transferObject->getHeaders(),
+            $transferObject->getBody()
+        );
+
+        try {
+            $response = $this->client->send($request);
+            $responseBody = json_decode($response->getBody(), true);
+            if ($responseBody['error_code'] == 0) {
+                return $responseBody;
+            }
+            else {
+                throw new ClientException(__($responseBody['error_message']));
+            }
+        } catch (GuzzleException $exception) {
+            throw new ClientException(__('Payment Gateway Error: ' . $exception->getMessage()));
+        }
+    }
+}
